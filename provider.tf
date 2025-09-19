845c75075d1cffe884cb24b541d17a79ac9ae752
babangol <maharshi1509@gmail.com>
2025-09-19 14:42:23 +0530

    Create provider.tf

---
+++ provider.tf
@@ -0,0 +1,15 @@
+terraform {
+  required_version = ">= 1.0.0"
+
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = ">= 3.0.0"
+    }
+  }
+}
+
+provider "azurerm" {
+  features {}
+  subscription_id = "78c8e033-d7c9-4fbd-9e14-093072a5948c"
+}
